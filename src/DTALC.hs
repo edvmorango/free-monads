6f896da0573b3b8685b0304f84dce52c0bf113a7
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-01-04 16:49:08 -0200

    feature: foldExp

--- src/DTALC.hs
+++ src/DTALC.hs
@@ -35,11 +35,39 @@ data (f :+: g) e
   = L (f e)
   | R (g e)
 
--- 10 + 10
+-- 10 + 11
 -- In (f (Exp f))
 -- In (LR (Exp LR))
 -- In (R (Add (Exp L) (Exp L)))
--- In (R (In ( Add (L (Exp L))) (In (L (Exp L))))) 
+-- In (R ( Add (In (L (Exp L))) (In (L (Exp L))))) 
 -- In before a applied Exp 
 addExample :: Exp (Val :+: Add)
-addExample = In (R ((Add (In (L (Val 10))) (In (L (Val 10))))))
+addExample = In (R ((Add (In (L (Val 10))) (In (L (Val 11))))))
+
+-- Right now Val is binded to a Int
+-- So  (a -> b) cannot exist just endomorphisms. 
+instance Functor Val where
+  fmap f (Val a) = Val a
+
+instance Functor Add where
+  fmap f (Add a a') = Add (f a) (f a')
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+  fmap f (L e) = L (f <$> e)
+  fmap f (R e) = R (f <$> e)
+
+-- foldExp f (In (R (Add ( In (L (Val 10))) (In (L (Val 11))))))
+-- foldExp f (In t) = f $ (foldExp f) <$> (R (Add  (In (L (Val 10))) (In (L (Val 11)))))
+-- foldExp f (In t) = f $ (foldExp f) <$> (L (Val 10))
+--					  (L (Val 11)) -- this product is parallelizable, so all products can be?
+--  f contains the evaluation, so:
+--  fold removes the `In` prefix
+--  f evaluate the expression
+--
+--		*
+--	*		*
+--   *     * 	     *	   *      	
+--
+--Evaluation will always occur first in the end of branchs.
+foldExp :: Functor f => (f a -> a) -> Exp f -> a
+foldExp f (In t) = f $ (foldExp f) <$> t
